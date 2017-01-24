ebd8cd4346c313d4ddd028c99d599cbf0097aece
manuel <manuel.otero@line64.com>
2017-01-24 16:59:46 -0300

    server start

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var port = process.env.PORT || 3000 ;
 
 // Public path configuration
 app.use(express.static(__dirname + '/build'));
-app.get('/', ()=> console.log("geting"))
+
 // Start App
 console.log('Starting landricks app... \nlistening on port ' + port);
 app.listen(port);
